b8aac05064d2b63038f25a89ec08f21ae3aa6e7a
moshik <moshik0527669169@gmail.com>
2025-05-21 23:06:19 +0300

    clean main

--- Program.cs
+++ Program.cs
@@ -4,23 +4,7 @@
     {
         static void Main(string[] args)
         {
-            F16 s = new F16("a", 10, 3000, "d");
-            //Console.WriteLine(s.Name);
-            //s.Attack(1);
-            //Console.WriteLine(s.refueling.ToString());
-            //s.fuel(2);
-            //Console.WriteLine(s.refueling.ToString());
-            //Console.WriteLine(s.FuelInTheTank);
-            //Console.WriteLine(DateTime.Now.ToString());
-            //Console.WriteLine(s.munitions(10));
-            //Console.WriteLine(s.armament.ToString());
 
-            s.Attack(1, 3);
-            s.refueling.ToString();
-            Console.WriteLine(s.refueling.ToString());
-            s.Attack(1, 4);
-            ZIK x = new ZIK("a", 3, 200, "5");
-            x.Attack(1,2);
         }
     }
 
