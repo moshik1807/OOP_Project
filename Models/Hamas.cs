5a508f01eb9dbdd67a264dacc42e616f815361fd
mendi eisenbach <menachemaiz.gmail.com>
2025-05-21 10:05:59 +0300

    test

---
+++ Models/Hamas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace IDFProject.Models
+{
+    public class Hamas
+    {
+        public string DateOfFormation { get; set; }
+        public string CurrentCommander { get; set; }
+        public List<Terrorist> terrorists { get; set; } = new List<Terrorist>();
+
+    }
+}
---
+++ Models/Terrorist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDFProject.Models
+{
+    public class Terrorist
+    {
+        public string Name { get; set; }
+        public  int Rank { get; set; }
+        public bool IsAlive { get; set; }
+        public List<string> Weapons { get; set; } = new List<string>();
+    }
+}
