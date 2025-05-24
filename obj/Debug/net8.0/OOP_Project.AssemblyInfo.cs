66072a0b2f0327e5b9c5865afac619d308afcc00
moshik <moshik0527669169@gmail.com>
2025-05-25 00:10:23 +0300

    add the AvailabilityOfTools

--- obj/Debug/net8.0/OOP_Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/OOP_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bfa7febaf8528a430a0267946b956d54f03949a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03f2a5b4a842886ef9d760dc7bce47ab6f4ba38b")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
