74e982b9879d296fb8604cdbae21bff00b5fbbde
mendi eisenbach <menachemaiz.gmail.com>
2025-05-26 17:50:59 +0300

    fix the PreferredTarget

--- obj/Debug/net8.0/OOP_Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/OOP_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56d77e4fdfa02d96cd8e8a7b3544e0d9068529c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f72c3fb4a4ddbf5c2379b9008bfd83bcdd09b56b")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
